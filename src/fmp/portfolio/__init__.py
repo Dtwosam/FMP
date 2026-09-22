783a50848db03f8974eceaeb4ce429c8f4aa35b0
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-09-22 02:00:59 +0100

    feat: export Phase 8A historical inventory

--- src/fmp/portfolio/__init__.py
+++ src/fmp/portfolio/__init__.py
@@ -10,6 +10,7 @@ from .contracts import (
     StrategyRecord,
     StrategyVersion,
 )
+from .historical_inventory import build_phase4_baseline_inventory
 from .registry import freeze_shadow_champion_set, transition_strategy
 from .router import route_shadow_candidates
 
@@ -24,6 +25,7 @@ __all__ = [
     "StrategyLifecycle",
     "StrategyRecord",
     "StrategyVersion",
+    "build_phase4_baseline_inventory",
     "freeze_shadow_champion_set",
     "route_shadow_candidates",
     "transition_strategy",
