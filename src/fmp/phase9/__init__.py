755fab4cfd54826fbae009b6bbe2e922ec9926af
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-09-22 18:13:46 +0100

    feat: export Phase 9 demo-design APIs

---
+++ src/fmp/phase9/__init__.py
@@ -0,0 +1,25 @@
+from .design import (
+    PHASE9_DECISION,
+    PHASE9_DEMO_DESIGN_ARTIFACT_PROTOCOL,
+    PHASE9_DEMO_DESIGN_FROZEN,
+    PHASE9_DEMO_DESIGN_PROTOCOL,
+    PHASE9_EXPERIMENT_ID,
+    PHASE9_MT5_ORDER_BRIDGE_PROTOCOL,
+    build_phase9_demo_design,
+    build_phase9_demo_design_from_campaign,
+    validate_phase9_demo_design,
+    write_phase9_demo_design,
+)
+
+__all__ = [
+    "PHASE9_DECISION",
+    "PHASE9_DEMO_DESIGN_ARTIFACT_PROTOCOL",
+    "PHASE9_DEMO_DESIGN_FROZEN",
+    "PHASE9_DEMO_DESIGN_PROTOCOL",
+    "PHASE9_EXPERIMENT_ID",
+    "PHASE9_MT5_ORDER_BRIDGE_PROTOCOL",
+    "build_phase9_demo_design",
+    "build_phase9_demo_design_from_campaign",
+    "validate_phase9_demo_design",
+    "write_phase9_demo_design",
+]
