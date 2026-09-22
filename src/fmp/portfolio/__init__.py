b151c66033a7a0aad17c50a5b9a05442394adf8e
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-09-22 09:32:58 +0100

    feat: export Phase 8A joint portfolio research API

--- src/fmp/portfolio/__init__.py
+++ src/fmp/portfolio/__init__.py
@@ -11,6 +11,11 @@ from .contracts import (
     StrategyVersion,
 )
 from .historical_inventory import build_phase4_baseline_inventory
+from .joint_research import (
+    JOINT_PORTFOLIO_PROTOCOL,
+    Phase8AJointPortfolioPlan,
+    run_phase8a_joint_portfolio,
+)
 from .registry import freeze_shadow_champion_set, transition_strategy
 from .research_batch import (
     PHASE8A_BATCH_ARTIFACT_PROTOCOL,
@@ -31,10 +36,15 @@ from .research_data import (
 from .research_runner import (
     Phase8ARetrospectivePlan,
     build_strategy_config,
+    generate_strategy_candidates,
     run_phase8a_retrospective_strategy,
     summarize_daily_returns,
 )
-from .router import route_shadow_candidates
+from .router import (
+    partition_direction_conflicts,
+    route_shadow_candidates,
+    summarize_candidate_exposure,
+)
 
 __all__ = [
     "PHASE8A_BATCH_ARTIFACT_PROTOCOL",
@@ -46,11 +56,13 @@ __all__ = [
     "CandidateRejection",
     "CandidateRejectionCode",
     "ChampionSet",
+    "JOINT_PORTFOLIO_PROTOCOL",
     "LoadedRetrospectiveBars",
     "PortfolioCandidate",
     "PortfolioExposure",
     "PortfolioRouteResult",
     "Phase8ABatchPlan",
+    "Phase8AJointPortfolioPlan",
     "Phase8ARetrospectivePlan",
     "RetrospectiveRange",
     "StrategyLifecycle",
@@ -58,12 +70,16 @@ __all__ = [
     "StrategyVersion",
     "build_phase4_baseline_inventory",
     "build_strategy_config",
+    "generate_strategy_candidates",
     "freeze_shadow_champion_set",
     "load_phase8a_retrospective_bars",
+    "partition_direction_conflicts",
     "route_shadow_candidates",
+    "run_phase8a_joint_portfolio",
     "run_phase8a_retrospective_batch",
     "run_phase8a_retrospective_strategy",
     "select_historical_inventory",
+    "summarize_candidate_exposure",
     "summarize_daily_returns",
     "write_phase8a_batch_artifacts",
     "transition_strategy",
